21b26aa39598c7fc9ac3a7746650a9cb7988fd6c
Vuyo_M_ Buthelezi <vuyanimondlibuthelezi@gmail.com>
2021-06-20 00:59:07 +0200

    app runs in terminal using maven

---
+++ target/classes/db.migration/Greet_add_users.sql
@@ -0,0 +1,4 @@
+insert into greet (username, counter) values ('Vuyo', 1 );
+insert into greet (username, counter) values ('Sam', 1 );
+insert into greet (username, counter) values ('Sese', 1 );
+insert into greet (username, counter) values ('Sim', 1 );
---
+++ target/classes/db.migration/Greet_create_table.sql
@@ -0,0 +1,5 @@
+create table greet (
+                id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+                username varchar(40),
+                counter int
+);
